a20025658af06f3be1abe88fab0835fd7e872ef1
fguerin93 <fleixjguerin@outlook.fr>
2018-12-14 10:06:28 +0100

    :hankey: remove canvas

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+
+//DOM ELEMENTS
+
+const $loadingContainer = document.querySelector('.loading-container')
+const $sentenceContainer = $loadingContainer.querySelector('.loading-container .sentence-container')
+const $sentences = $sentenceContainer.querySelectorAll('.loading-container .sentence')
+
+const $skipButton = $loadingContainer.querySelector('.loading-container .ski-button')
+const $loadBar = $loadingContainer.querySelector('.loading-container .load-bar')
